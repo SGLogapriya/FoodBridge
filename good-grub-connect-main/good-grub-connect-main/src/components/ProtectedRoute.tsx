13b9e8ea18e0a4e71abe61a1c5ba6407936d3051
SGLogapriya <230701164@rajalakshmi.edu.in>
2026-04-14 16:51:28 +0530

    frontend update

--- good-grub-connect-main/good-grub-connect-main/src/components/ProtectedRoute.tsx
+++ good-grub-connect-main/good-grub-connect-main/src/components/ProtectedRoute.tsx
@@ -1,29 +1,22 @@
 import { Navigate } from "react-router-dom";
 import { ReactNode } from "react";
 
-interface Props {
-  children: ReactNode;
-  role?: "donor" | "ngo";
-}
 
-const ProtectedRoute = ({ children, role }: Props) => {
-  const userData = localStorage.getItem("user");
+const ProtectedRoute = ({ children, role }: any) => {
+  const user = JSON.parse(localStorage.getItem("user") || "null");
 
   // ❌ Not logged in
-  if (!userData) {
-    return <Navigate to="/login" replace />;
+  if (!user) {
+    return <Navigate to="/login"/>;
   }
-
-  const user = JSON.parse(userData);
-
   // ❌ Wrong role trying to access
   if (role && user.role !== role) {
     alert("Access denied! Wrong role.");
-    return <Navigate to="/" replace />;
+    return <Navigate to="/login"/>;
   }
 
   // ✅ Allowed
-  return <>{children}</>;
+  return children;
 };
 
 export default ProtectedRoute;
