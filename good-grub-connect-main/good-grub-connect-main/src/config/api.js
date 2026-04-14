6298e2f93f829e6c7518e36b5f236e1ffbf93709
SGLogapriya <230701164@rajalakshmi.edu.in>
2026-04-14 16:58:06 +0530

    frontend update2

--- good-grub-connect-main/good-grub-connect-main/src/config/api.js
+++ good-grub-connect-main/good-grub-connect-main/src/config/api.js
@@ -1,4 +1,4 @@
 const API_BASE_URL =
-  "https://foodbridge-backend-api-gmfpbacnfvhbghf7.centralindia-01.azurewebsites.net/api";
+  "https://foodbridge-backend-api-gmfpbacnfvhbghf7.centralindia-01.azurewebsites.net";
 
 export default API_BASE_URL;
